8ba221c731efa3b9892babbd501281a7bd5a494d
Baptiste <constant.baptiste@lyceeppr.fr>
2020-02-27 15:31:35 +0100

    suppression ancien version passage à Symfony 5.0

--- SiteWebCdG/footer.php
+++
@@ -1,4 +0,0 @@
-<h6 class="h6 col-sm-12">Copyright &copy; Ce que vous voulez Designed by Baptiste Constant
-Valid
-<a href="http://jigsaw.w3.org/css-validator/">CSS</a> &amp;
-<a href="http://validator.w3.org/">HTML5</a></p>
--- SiteWebCdG/process-supptemplate.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $resultat = mysqli_query($db,"DELETE FROM doc WHERE id =".$_POST['suppTemplate']) or die ("requète non executé");
-  header ("location: confirmation-supp.php")
-?>
